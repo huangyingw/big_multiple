943b4baa2bcbecec1d95f2ee80cfca7ed5deb5f8
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-09 15:49:24 +0800

    n

--- big_multiple.cc
+++ big_multiple.cc
@@ -19,7 +19,7 @@ long mult(long x,long y,int n)
     b=long(x%long(pow(10,(n/2)))); //取x的右半部分
     c=long(y/pow(10,(n/2))); //取y的左半部分
     d=long(y%long(pow(10,(n/2)))); //取y的右半部分
-    s=mult(a,c,n)*pow(2,n)+(mult((a-b),(d-c),n)+mult(a,c,n)+mult(b,d,n))*pow(2,n/2)+mult(b,d,n); //书上的公式
+    s=mult(a,c,n/2)*pow(10,n)+(mult(a,d,n/2)+mult(b,c,n/2))*pow(10,n/2)+mult(b,d,n/2); //书上的公式
     return (s);
   }
 }
@@ -42,7 +42,7 @@ int num(long x) //判断输入的数字的位数
 
 int main() //主函数
 {
-  long x=123,y=256;
+  long x=1234,y=2564;
   cout<<mult(x,y,num(x))<<endl;
   return 0;
 }
