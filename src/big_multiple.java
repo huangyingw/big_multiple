d220306adae9b2d00a2c103c805d3d0d473760b9
huangyingw@linux_programming <huangyingw@gmail.com>
2012-01-30 18:01:28 +0800

    n

--- src/big_multiple.java
+++ src/big_multiple.java
@@ -3,26 +3,28 @@
  * 
  */
 public class big_multiple {
-	static long mult(long x, long y, int n) {
+	static long mult(long x, long y, int nx, int ny) {
 		long a, b, c, d, s;
-		if (n == 1) // 起初这里落了个＝号
+		if (nx == 1 && ny == 1) // 起初这里落了个＝号
 			return x * y;
 		else {
-			a = (long) (x / Math.pow(10, (n / 2))); // 取x的左半部分
-			b = (long) (x % (Math.pow(10, (n / 2)))); // 取x的右半部分
-			c = (long) (y / Math.pow(10, (n / 2))); // 取y的左半部分
-			d = (long) (y % (Math.pow(10, (n / 2)))); // 取y的右半部分
-			s = (long) (mult(a, c, n / 2) * Math.pow(10, n)
-					+ (mult(a, d, n / 2) + mult(b, c, n / 2))
-					* Math.pow(10, n / 2) + mult(b, d, n / 2)); // 书上的公式
+			a = (long) (x / Math.pow(10, (nx / 2))); // 取x的左半部分
+			b = (long) (x % (Math.pow(10, (nx / 2)))); // 取x的右半部分
+			c = (long) (y / Math.pow(10, (ny / 2))); // 取y的左半部分
+			d = (long) (y % (Math.pow(10, (ny / 2)))); // 取y的右半部分
+			s = (long) (mult(a, c, nx / 2, ny / 2)
+					* Math.pow(10, (nx + ny) / 2)
+					+ mult(a, d, nx / 2, ny - ny / 2) * Math.pow(10, nx / 2)
+					+ mult(b, c, nx - nx / 2, ny / 2) * Math.pow(10, ny / 2) + mult(
+					b, d, nx - nx / 2, ny - ny / 2)); // 书上的公式
 			return (s);
 		}
 	}
 
-	int num(long x) // 判断输入的数字的位数
+	static int num(long x) // 判断输入的数字的位数
 	{
 		int i = 0;
-		if (x - 9 <= 0)
+		if (x <= 9)
 			return 1;
 		else {
 			while (x != 0) {
@@ -35,6 +37,6 @@ public class big_multiple {
 
 	public static void main(String args[]) {
 		long x = 1234, y = 2564;
-		System.out.println(mult(x, y, 4));
+		System.out.println(mult(x, y, num(x), num(y)));
 	}
 }
