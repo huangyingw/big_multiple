5adb46e8c28e795c31195b7bf24fe75575e0d669
huangyingw@linux_programming <huangyingw@gmail.com>
2012-01-30 18:11:24 +0800

    n

--- src/big_multiple.java
+++ src/big_multiple.java
@@ -12,11 +12,10 @@ public class big_multiple {
 			b = (long) (x % (Math.pow(10, (nx / 2)))); // 取x的右半部分
 			c = (long) (y / Math.pow(10, (ny / 2))); // 取y的左半部分
 			d = (long) (y % (Math.pow(10, (ny / 2)))); // 取y的右半部分
-			s = (long) (mult(a, c, nx / 2, ny / 2)
-					* Math.pow(10, (nx + ny) / 2)
-					+ mult(a, d, nx / 2, ny - ny / 2) * Math.pow(10, nx / 2)
-					+ mult(b, c, nx - nx / 2, ny / 2) * Math.pow(10, ny / 2) + mult(
-					b, d, nx - nx / 2, ny - ny / 2)); // 书上的公式
+			s = (long) (mult(a, c, num(a), num(c))
+					* Math.pow(10, (nx + ny) / 2) + mult(a, d, num(a), num(d))
+					* Math.pow(10, nx / 2) + mult(b, c, num(b), num(c))
+					* Math.pow(10, ny / 2) + mult(b, d, num(b), num(d))); // 书上的公式
 			return (s);
 		}
 	}
@@ -38,5 +37,18 @@ public class big_multiple {
 	public static void main(String args[]) {
 		long x = 1234, y = 2564;
 		System.out.println(mult(x, y, num(x), num(y)));
+
+		x = 1234;
+		y = 25645;
+		System.out.println(mult(x, y, num(x), num(y)));
+
+		x = 3;
+		y = 25645;
+		System.out.println(mult(x, y, num(x), num(y)));
+
+		x = 1234892;
+		y = 25645;
+		System.out.println(mult(x, y, num(x), num(y)));
+
 	}
 }
